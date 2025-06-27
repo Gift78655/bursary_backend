5e1d0c998655a7ab3a197de6ab0576d6c240ea1a
Gift78655 <rethabilegift240@gmail.com>
2025-06-27 14:01:04 +0200

    Refactor: Modularize routes and email transport

---
+++ db.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2');
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  ssl: {
+    ca: fs.readFileSync(path.resolve(__dirname, process.env.DB_SSL_CERT))
+  }
+});
+module.exports = pool;
